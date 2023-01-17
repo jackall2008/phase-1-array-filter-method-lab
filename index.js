957b656318c8200e576c00f3b796210d2fc501ff
Julian Patterson <julian.patterson@gmail.com>
2023-01-17 15:28:28 -0500

    This file contains code passing 7 JavaScript array filter method tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(allNames, specificName) {
+    return allNames.filter(name => name.toLowerCase() === specificName.toLowerCase());
+}
+
+function fuzzyMatch(allNames, specificName) {
+    return allNames.filter(name => name.charAt(0) === specificName.charAt());
+}
+
+function matchName(allNames, specificName) {
+    return allNames.filter(name => name.name === specificName);
+}
